00b194d72490c89f54d2d38342aa891e995f689e
Naoki Ogawa <n-ogawa@sakura.ad.jp>
2020-01-27 16:25:25 +0900

    apply for prefix.yaml

--- rrdb.clj
+++ rrdb.clj
@@ -1,9 +1,23 @@
 (ns user (:require [net.cgrand.enlive-html :as html]))
-(def asnum 16503)
-(def radb-url (format "https://www.radb.net/query?advanced_query=1&keywords=as%d&-i=1&-i+option=origin" asnum))
-(def html-text (slurp radb-url))
-(def list-pre (-> html-text (html/html-snippet)(html/select [:pre])))
-(defn proc [x]
-    (second (clojure.string/split (first (clojure.string/split (first((first (x :content)) :content)) #"\n" )) #"\s+"))
+
+(defn radb-url [asnum] (format "https://www.radb.net/query?advanced_query=1&keywords=as%d&-i=1&-i+option=origin" asnum))
+;(defn radb-url [_] "localsample")
+
+(defn list-pre [asnum] (-> (slurp (radb-url asnum)) (html/html-snippet)(html/select [:pre])))
+
+(defn makeonemap [x]
+  (reduce (fn [h [_ k v]](assoc h k v)) {} (re-seq #"(.+):\s+(.+)(\n|$)" x))
 )
-(map proc list-pre)
+(defn proc [x]
+  ;(clojure.string/split (first (clojure.string/split (first((first (x :content)) :content)) #"\n" )) #"\s+")
+  ;(clojure.string/split (first((first (x :content)) :content)) #"\n" )
+  (let [o (makeonemap (first((first (x :content)) :content)))]
+     (if (get o "route" ) (println (format "%s:" (get o "route" ))))
+     (if (get o "route6") (println (format "%s:" (get o "route6"))))
+     (if (get o "descr" ) (println "    description:" (get o "descr") ))
+     (if (get o "origin" ) (println "    asn:" (clojure.string/replace (get o "origin") #"AS" "" )))
+     (println "    ignoreMorespecifics: false")
+  )
+)
+
+(doall (map proc (list-pre 16503)))
